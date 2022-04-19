e343052a2b25f32067a62c55f1793a18798c9972
kiliann <wilian_kilian@hotmail.com>
2022-04-19 20:43:23 +0200

    v1.1 login

--- src/js/app.js
+++ src/js/app.js
@@ -6,15 +6,46 @@ import {VistaPrincipal} from './vistas/vistaprincipal.js'
  *
  * @class App
  */
-class App{
-  constructor(){
-    window.onload = this.iniciar.bind(this)
-  }
-  iniciar(){
-    this.vistaPrincipal = new VistaPrincipal(this, document.body)
-  }
-  
+class App {
+    constructor() {
+        window.onload = this.iniciar.bind(this)
+    }
 
+    iniciar() {
+        this.vistaPrincipal = new VistaPrincipal(this, document.body)
+
+    }
+
+    loginUsuario() {
+        let u;
+        let p;
+
+        if (u !== "" && p != "") {
+            S.ajax(
+                {
+                    url: urlserver + "src/php/controlador/controladorBackend.php",
+                    type: "POST",
+                    data:
+                        {
+                            user: u,
+                            password: p
+                        },
+                    dataType: 'json',
+                    success: function (response) {
+                        //alert(response.success);
+                        if (response.success == true) {
+                            localStorage.setItem('sesion', u);// SE GUARDA DE SESION DEL USUARIO
+                            localStorage.setItem('us_nombre', response.us_nombre);// SE GUARDA DE SESION NOMBRE DE
+
+                            localStorage.setItem('us_id', response.us_id);
+                            S(location).attr('href', "inicio.html");
+                            mensaje("nsj_1login", "texto_login", "BIENVENIDO" + response.us_nombre, "success")
+                        } else {
+                            mensaje("msj_login", "texto_login", response.mensaje, "danger");
+                        }
+                    }
+                });
+        }
+    }
 }
-
 new App()
